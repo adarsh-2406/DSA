815217bf5463edf802336b79eea4a646e3fd0bb0
adarsh-2406 <adarshprao24@gmail.com>
2026-07-08 01:59:53 +0530

    Time: 0 ms (100%), Space: 19.4 MB (64.76%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -3,19 +3,20 @@ public:
     vector<int> twoSum(vector<int>& numbers, int target) {
         int i = 0;
         int j = numbers.size() - 1;
-        vector<int>ans(2,0);
+        // vector<int>ans(2,0); vector lene ki jrut nhi , ek hi answer hai
         while(i<j){
             
             if( numbers[i] + numbers[j] == target ){
-                ans[0] = i+1;
-                ans[1] = j+1;
-                break;
+                return {i+1 , j+1 };
+                // ans[0] = i+1;
+                // ans[1] = j+1;
+               
             }
             else if( numbers[i] + numbers[j] > target){
                 j--;
             }
             else i++;
         }
-        return ans;
+        return {-1,-1};
     }
 };
