0161970ce025a52e4d0355a3c7e3fbad173560d0
Adarsh Pratap Rao <adarshprao24@gmail.com>
2026-07-15 02:02:29 +0530

    Time: 0 ms (100%), Space: 8.6 MB (96.87%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.cpp
+++ 0441-arranging-coins/0441-arranging-coins.cpp
@@ -1,7 +1,25 @@
 class Solution {
 public:
-    int arrangeCoins(int n) {
-        
+    int arrangeCoins(int n) {       
+// using binary search;
+//         long long low = 0, high = n;
+
+//         while (low <= high) {
+//             long long mid = low + (high - low) / 2;
+//             long long coins = mid * (mid + 1) / 2;
+
+//             if (coins == n)
+//                 return mid;
+//             else if (coins < n)
+//                 low = mid + 1;
+//             else
+//                 high = mid - 1;
+//         }
+
+//         return high;
+//     }
+// };
+// using AP; solve the equation;
         return  (sqrt(8.0*n +1) - 1)/2;
     }
 };
