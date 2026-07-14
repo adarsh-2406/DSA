01dd47d4eb583f25f0e048ed5ba0533a8af9de4f
Adarsh Pratap Rao <adarshprao24@gmail.com>
2026-07-14 20:35:40 +0530

    Time: 0 ms (100%), Space: 9.3 MB (82.36%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -2,6 +2,11 @@ class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
         vector<int>ans;
+        // left se right jao 
+        // top - 1 se bottom
+        // right - 1 se left
+        // bottom - 1 se top - 1 tk
+        // aur edge case chevk kro ki same line dubara toh nhi kr rha 
         int left = 0 , right = matrix[0].size() - 1;
         int top = 0 , bottom = matrix.size() - 1;
         while(left <= right && top <= bottom){
@@ -12,11 +17,11 @@ public:
             ans.push_back(matrix[i][right]);
         }
         for(int i = right - 1; i>=left; i--){
-            if(top == bottom) break;
+            if(top == bottom) break; // edge case; 
             ans.push_back(matrix[bottom][i]);
         }
         for(int i = bottom-1 ; i>top ; i--){
-            if(left == right) break;
+            if(left == right) break; // edge case;
             ans.push_back(matrix[i][left]);
         }
         left++;
