90bb55784702e74a186613545e357698ae897d40
Adarsh Pratap Rao <adarshprao24@gmail.com>
2026-07-22 22:40:04 +0530

    Time: 0 ms (100%), Space: 17.9 MB (99.04%) - LeetHub

---
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int low = 0;
+        int high = n-1;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            if(nums[mid] == target) return true;
+            // check edge case for duplicate , aggr low , mid , high tino same ho jaayega toh hum check nhi kr payenge ki konsa part sorted hai;
+            else if(nums[low] == nums[mid] && nums[mid] == nums[high]){ 
+                low++; 
+                high--;
+            }
+            else if(nums[low] <= nums[mid]){ // checking left half sorted or not;
+                if(nums[low] <= target && target<=nums[mid]){ // checking target lies in left or not;
+                    high = mid-1;
+                }
+                else low = mid+1;
+            }
+            else{
+                if(nums[mid]<=target && target<=nums[high]){ // checking target lies in right or not;
+                    low = mid+1;
+                }
+                else high = mid-1;
+            }
+        }
+        return false;
+    }
+};
